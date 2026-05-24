395e3da1ca5498dd8a43490d74a25f03105062ad
Aman Maurya <amanmaurya7266@gmail.com>
2026-05-25 03:42:32 +0530

    email added

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { trigger, transition, style, animate } from '@angular/animations';
-
+import emailjs from '@emailjs/browser';
 interface ContactForm {
   name: string;
   email: string;
@@ -40,14 +40,53 @@ export class ContactComponent {
     if (this.isSubmitting()) return;
 
     this.isSubmitting.set(true);
-
+    this.sendEmail();
     // Simulate API call
-    setTimeout(() => {
-      this.isSubmitting.set(false);
-      this.isSubmitted.set(true);
+    // setTimeout(() => {
+    //   this.isSubmitting.set(false);
+    //   this.isSubmitted.set(true);
 
-      // Reset form
-      this.form = { name: '', email: '', subject: '', message: '' };
-    }, 1500);
+    //   // Reset form
+    //   this.form = { name: '', email: '', subject: '', message: '' };
+    // }, 1500);
+  }
+
+  sendEmail(){
+    debugger;
+      if (
+    !this.form.name.trim() ||
+    !this.form.email.trim() ||
+    !this.form.subject.trim() ||
+    !this.form.message.trim()
+  ) {
+    alert('Please fill all fields');
+    this.isSubmitting.set(false);
+    return;
+  }
+     const templateParams = {
+      name: this.form.name.trim(),
+      email: this.form.email.trim(),
+      message: this.form.message.trim(),
+      title:this.form.subject.trim()
+    };
+    emailjs.send(
+      'service_rqnscgg',
+      'template_uxxizdk',
+      templateParams,
+      '8Q_tRL1Hj3_V329Kq'
+    ).then(
+      (response) => {
+
+        this.isSubmitting.set(false);
+        this.isSubmitted.set(true);
+        this.form = { name: '', email: '', subject: '', message: '' };
+
+      },
+      (error) => {
+
+        alert('Failed To Send Email');
+        this.isSubmitting.set(false);
+      }
+    );
   }
 }
